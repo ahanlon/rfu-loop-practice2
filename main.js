1555ffd80526d1054cc67f42aff5d1d26c84e8c0
ahanlon <hanlonad27@gmail.com>
2015-06-29 19:18:16 -0600

    final save

--- main.js
+++ main.js
@@ -4,19 +4,19 @@
 
 var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot']
 
-// for (var i=0; i<(animals.length - 1); i++) {
-// 	console.log(animals[i]);
-// }
+for (var i=0; i<(animals.length - 1); i++) {
+	console.log(animals[i]);
+}
 
-// //Problem 2.
-// for (var i=0; i<(animals.length); i+=2) {
-// 	console.log(animals[i]);
-// }
+//Problem 2.
+for (var i=0; i<(animals.length); i+=2) {
+	console.log(animals[i]);
+}
 
-// // Problem 3.
-// for (var i=(animals.length -1); i>=0; i--){
-// 	console.log(animals[i]);
-// };
+// Problem 3.
+for (var i=(animals.length -1); i>=0; i--){
+	console.log(animals[i]);
+};
 
 //Problem 4.
 
